82ffd85148b43c7c4cf1c68a175b23e3ab721f66
qianfeng <qianfeng@wy21.local>
2014-12-26 16:46:33 +0800

    Initial Commit

---
+++ autoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autoPlayer
+//
+//  Created by qianfeng on 14-12-26.
+//  Copyright (c) 2014年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
